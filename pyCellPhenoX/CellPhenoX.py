471e3b120a17d8c7dff0b572d58b03ef41d22233
reyvnth <therevanthkrishna@gmail.com>
2025-07-21 17:26:39 -0600

    v1.4 Release

--- pyCellPhenoX/CellPhenoX.py
+++ pyCellPhenoX/CellPhenoX.py
@@ -1,9 +1,15 @@
-# In[1]: Imports
+####################################################
+###
+###                     IMPORTS
+###
+####################################################
 
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 import shap
+import fasttreeshap
+import time
 from xgboost import *
 
 from sklearn.model_selection import train_test_split
@@ -24,8 +30,11 @@ from sklearn.preprocessing import LabelEncoder
 
 np.random.seed(1)
 
-# In[2]: CellPhenoX Class
-
+####################################################
+###
+###                     CLASS
+###
+####################################################
 
 class CellPhenoX:
     def __init__(self, X, y, CV_repeats, outer_num_splits, inner_num_splits):
@@ -41,6 +50,7 @@ class CellPhenoX:
         # self.steps = ["hyperparameter tuning", "model training", "model prediction", "performance eval", "SHAP value calculation"]
         # self.CV_repeat_cumulative_times = [0,0,0,0,0]
         # self.CV_repeat_times = []
+        self.model_training_time = None
         self.X = X
         self.y = y
         # self.fc = fc
@@ -59,6 +69,8 @@ class CellPhenoX:
                 "n_estimators": [100, 200, 800],
             }
         ]
+        self.roc_curves = [] # list of dictionaries with ROC curve datra for each CV repeat
+        self.prc_curves = [] # list of dictionaries with the PRC curve data for each CV repeat
         self.label_encoder = LabelEncoder()
         self.best_model = None
         self.best_score = float("-inf")
@@ -104,17 +116,29 @@ class CellPhenoX:
             y_test_outer,
             y_val_inner,
         ]
+    
+    def shap_values_explainer(self, model, X, fast=True, n_jobs=-1):
+        if fast:
+            explainer = fasttreeshap.TreeExplainer(model, algorithm="auto", n_jobs=n_jobs) # n_jobs=-1 for parallel processing
+            shap_values = explainer(X).values
+        else:
+            explainer = shap.TreeExplainer(model)
+            shap_values = explainer.shap_values(X)
+        
+        return shap_values
 
-    def model_training_shap_val(self, outpath):
+    def model_training_shap_val(self, fast, outpath):
         """Train the model using nested cross validation strategy and generate shap values for each fold/CV repeat
 
         Parameters:
+        fast (bool): whether to use the fasttreeshap package or the shap package
         outpath (str): the path for the output folder
 
         Returns:
 
 
         """
+        start_time = time.time()
 
         fig, axes = plt.subplots(nrows=1, ncols=3, figsize=(18, 5))
         accuracy_list = []
@@ -183,13 +207,13 @@ class CellPhenoX:
                     n_jobs=1,
                 )  # -#-#
                 result = search.fit(X_train_inner, y_train_inner)  # -#=#
-
+                
                 # Fit model on training data
                 result.best_estimator_.fit(X_train_outer, y_train_outer)  # -#-#
 
                 # Make predictions on the test set
                 y_pred = result.best_estimator_.predict(X_test_outer)
-                y_prob = result.best_estimator_.predict_proba(X_test_outer)  # [:, 1]
+                y_prob = result.best_estimator_.predict_proba(X_test_outer)#[:, 1]
                 y_prob_list.append(y_prob)
                 y_test_list.append(y_test_outer)
 
@@ -199,45 +223,38 @@ class CellPhenoX:
                 accuracy_list.append(accuracy)
                 y_val_pred = result.best_estimator_.predict(X_val_inner)
                 val_accuracy = accuracy_score(y_val_inner, y_val_pred)
-                # val_accuracy_list.append(val_accuracy_list)
+                #val_accuracy_list.append(val_accuracy_list)
                 val_accuracy_list.append(val_accuracy)
 
-                # Calculate ROC curve and AUC
+                # Calculate ROC curve and AUC 
                 num_classes = len(np.unique(y_train_outer))
                 if num_classes == 2:
-                    # fpr, tpr, thresholds = roc_curve(y_test_outer, y_pred)
-                    # auc_value = auc(fpr, tpr)
-                    # auc_list.append(auc_value)
+                    #fpr, tpr, thresholds = roc_curve(y_test_outer, y_pred)
+                    #auc_value = auc(fpr, tpr)
+                    #auc_list.append(auc_value)
                     # Validate on the validation set
                     y_prob_val = result.best_estimator_.predict_proba(X_val_inner)[:, 1]
                     fpr_val, tpr_val, _ = roc_curve(y_val_inner, y_prob_val)
                     val_auc = auc(fpr_val, tpr_val)
                     val_prc = average_precision_score(y_val_inner, y_prob_val)
-                else:  # more than 2 classes
+                else: # more than 2 classes
                     val_auc = []
                     val_prc = []
                     for k in range(num_classes):
-                        # fpr, tpr, _ = roc_curve(y_test_outer == k, y_prob[:, k])
-                        # auc_value = auc(fpr, tpr)
-                        # auc_list.append(auc_value)
-                        val_fpr, val_tpr, _ = roc_curve(
-                            y_val_inner == k,
-                            result.best_estimator_.predict_proba(X_val_inner)[:, k],
-                        )
+                        #fpr, tpr, _ = roc_curve(y_test_outer == k, y_prob[:, k])
+                        #auc_value = auc(fpr, tpr)
+                        #auc_list.append(auc_value)
+                        val_fpr, val_tpr, _ = roc_curve(y_val_inner == k, result.best_estimator_.predict_proba(X_val_inner)[:, k])
                         val_auc.append(auc(val_fpr, val_tpr))
-                        val_prc.append(
-                            average_precision_score(
-                                y_val_inner == k,
-                                result.best_estimator_.predict_proba(X_val_inner)[:, k],
-                            )
-                        )
-
+                        val_prc.append(average_precision_score(y_val_inner == k, result.best_estimator_.predict_proba(X_val_inner)[:, k]))
+                    
+                
                 val_auc_list.append(val_auc)
-                # print(len(val_auc_list))
+                #print(len(val_auc_list))
                 val_prc_list.append(val_prc)
                 val_auc_combined_list.append(val_auc)
                 val_prc_combined_list.append(val_prc)
-                # val_accuracy_list.append(val_accuracy)
+                #val_accuracy_list.append(val_accuracy)
                 print(
                     "--- Validation Accuracy: ",
                     val_accuracy,
@@ -246,49 +263,52 @@ class CellPhenoX:
                     " - Val AUPRC: ",
                     val_prc,
                 )
-
-                explainer = shap.TreeExplainer(result.best_estimator_)
-                shap_values = explainer.shap_values(X_test_outer)
+                
+                # Calculate SHAP values
+                # Using TreeExplainer from shap package
+                #explainer = shap.TreeExplainer(result.best_estimator_)
+                #shap_values = explainer.shap_values(X_test_outer)
+                # Using fasttreeshap 
+                # explainer = fasttreeshap.TreeExplainer(result.best_estimator_, algorithm="auto", n_jobs=-1) # n_jobs=-1 for parallel processing
+                # shap_values = explainer(X_test_outer).values
+                shap_values = self.shap_values_explainer(result.best_estimator_, X_test_outer, fast=fast, n_jobs=-1)
+                # print("shap values type:", type(shap_values))
+                # print("shap values shape:", np.array(shap_values).shape)
 
                 # Extract SHAP information per fold per sample
-                # print(shap_values.shape)
+                #print(shap_values.shape)
                 for k, test_index in enumerate(test_outer_ix):
                     test_index = self.X.index[test_index]
                     # here, I am selecting the second (1) shap array for a binary classification problem.
                     # we need a way to generalize this so that we select the array that corresponds to the
                     # positive class (disease).
                     if num_classes == 2:
-                        self.shap_values_per_cv[test_index][CV_repeat] = shap_values[0][
-                            k
-                        ]
+                        if fast: # fasttreeshap returns a 3D array
+                            self.shap_values_per_cv[test_index][CV_repeat] = shap_values[k, : , 0]
+                        else: # the shap package returns a list of two arrays
+                            self.shap_values_per_cv[test_index][CV_repeat] = shap_values[0][k]
                     else:
                         predicted_class = y_pred[k]
-                        self.shap_values_per_cv[test_index][CV_repeat] = shap_values[
-                            predicted_class
-                        ][k]
+                        self.shap_values_per_cv[test_index][CV_repeat] = shap_values[predicted_class][k]
 
                 # save best model
                 model_list.append(result.best_estimator_)
 
             # one ROC curve for each repeat
-            # y_prob_combined = np.concatenate(y_prob_list)
-            y_prob_combined = (
-                np.concatenate([prob[:, 1] for prob in y_prob_list])
-                if num_classes == 2
-                else np.concatenate(y_prob_list)
-            )
+            #y_prob_combined = np.concatenate(y_prob_list)
+            y_prob_combined = np.concatenate([prob[:, 1] for prob in y_prob_list]) if num_classes == 2 else np.concatenate(y_prob_list)
             y_test_combined = np.concatenate(y_test_list)
             y_prob_combined_list.append(y_prob_combined)
             y_test_combined_list.append(y_test_combined)
 
-            # val_accuracy_combined = val_accuracy_list
-            # val_accuracy_combined_list.append(val_accuracy_combined)
-            # val_prc_combined = val_prc_list
-            # val_prc_combined_list.append(val_prc_combined)
+            #val_accuracy_combined = val_accuracy_list
+            #val_accuracy_combined_list.append(val_accuracy_combined)
+            #val_prc_combined = val_prc_list
+            #val_prc_combined_list.append(val_prc_combined)
 
             precision, recall, _ = precision_recall_curve(
-                y_test_combined, y_prob_combined
-            )
+                            y_test_combined, y_prob_combined
+                        )
             prc_auc = average_precision_score(y_test_combined, y_prob_combined)
 
             # BINARY if statement here?
@@ -300,37 +320,30 @@ class CellPhenoX:
                 # Compute AUC (Area Under the Curve)
                 roc_auc = auc(fpr, tpr)
                 # Plot the ROC and precision recall curve for the current fold size
-                axes[0].plot(
-                    fpr, tpr, lw=2, label=f"CV Repeat {i+1} (ROC = {roc_auc:.2f})"
-                )
+                axes[0].plot(fpr, tpr, lw=2, label=f"CV Repeat {i+1} (ROC = {roc_auc:.2f})")
+                self.roc_curves.append({'fpr': fpr, 'tpr': tpr, 'roc_auc': roc_auc})
             else:
                 # MULTICLASS else statement Here?
                 for k in range(num_classes):
-                    fpr, tpr, _ = roc_curve(
-                        y_test_combined == k, y_prob_combined[:, k]
-                    )  # one vs rest..
+                    fpr, tpr, _ = roc_curve(y_test_combined == k, y_prob_combined[:, k]) # one vs rest..
                     roc_auc = auc(fpr, tpr)
-                    axes[0].plot(
-                        fpr,
-                        tpr,
-                        lw=2,
-                        label=f"CV Repeat {i+1} - Class {k} (ROC = {roc_auc:.2f})",
-                    )
-
+                    axes[0].plot(fpr, tpr, lw=2, label=f"CV Repeat {i+1} - Class {k} (ROC = {roc_auc:.2f})")
+                    self.roc_curves.append({'fpr': fpr, 'tpr': tpr, 'roc_auc': roc_auc}) 
             # Precision Recall curve
             axes[1].plot(
-                recall, precision, lw=2, label=f"CV Repeat{i+1} (PRC = {prc_auc:.2f})"
-            )
+                            recall, precision, lw=2, label=f"CV Repeat{i+1} (PRC = {prc_auc:.2f})"
+                        )
+            self.prc_curves.append({'recall': recall, 'precision': precision, 'prc_auc': prc_auc})
             # save the best model for this repeat
-            model_pr_pairs = list(
-                zip(model_list, val_prc_combined_list)
-            )  # val_prc_combined
+            model_pr_pairs = list(zip(model_list, val_prc_combined_list)) #val_prc_combined
 
             # Find the model with the highest precision-recall score
             best_model_repeat, best_score_repeat = max(
                 model_pr_pairs, key=lambda x: x[1]
             )
             overal_model_list.append((best_model_repeat, best_score_repeat))
+
+            
 
         # Add labels and show the ROC curve plot
         axes[0].plot([0, 1], [0, 1], color="navy", lw=2, linestyle="--")
@@ -399,6 +412,14 @@ class CellPhenoX:
         # and calculate the interpretable score
         self.get_interpretable_score()
 
+        end_time = time.time()
+        self.model_training_time = end_time - start_time
+        model_training_time_min = self.model_training_time / 60
+        print(f"Total training time for {self.CV_repeats} CV repeat(s): {model_training_time_min:.2f} minutes")
+
+    def get_model_training_time(self):
+        return self.model_training_time
+
     def get_shap_values_per_cv(self):
         return self.shap_values_per_cv
 
@@ -412,8 +433,8 @@ class CellPhenoX:
         average_shap_values = []
         for i in range(0, len(self.X)):  # len(NAM)
             id = self.X.index[i]  # NAM.index[i]
-            # print(id)
-            # print(self.shap_values_per_cv)
+            #print(id)
+            #print(self.shap_values_per_cv)
             df_per_obs = pd.DataFrame.from_dict(
                 self.shap_values_per_cv[id][0]
             )  # Get all SHAP values for sample number i
@@ -434,3 +455,11 @@ class CellPhenoX:
         interpretable_score = np.sum(self.shap_df, axis=1)
         # add the shap_df
         self.shap_df["interpretable_score"] = interpretable_score
+
+    def get_roc_curves(self):
+        return self.roc_curves
+    
+    def get_prc_curves(self):
+        return self.prc_curves
+
+    
