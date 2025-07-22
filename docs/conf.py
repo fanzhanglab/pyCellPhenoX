cc2a5e457aec1059090fef9dc62d16b74243373e
reyvnth <therevanthkrishna@gmail.com>
2025-07-21 18:14:16 -0600

    v1.5 Release

--- docs/conf.py
+++ docs/conf.py
@@ -24,7 +24,7 @@ import pyCellPhenoX
 project = "pyCellPhenoX"
 copyright = "2025, Zhang Lab @ CU Medicine"
 author = "Fan Zhang, Jade Young, Jun Inamo, Revanth Krishna, Zachary Caterer"
-release = "1.4"
+release = "1.5"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
